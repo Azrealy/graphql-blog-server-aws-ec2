5f9e0caaae1b249ade6dd0b6f13dc5f5e162909f
Azrealy <fangkaihang1992@yahoo.co.jp>
2018-09-21 02:09:25 +0900

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 import 'dotenv/config';
+import http from 'http';
+import cors from 'cors';
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import { ApolloServer, AuthenticationError } from 'apollo-server-express';
 import DataLoader from 'dataloader';
+import { ApolloServer } from 'apollo-server-express';
+import { AuthenticationError } from 'apollo-server';
+
 import schema from './schema';
 import resolvers from './resolvers';
-import cors from 'cors';
-import http from 'http';
-import loaders from './loaders';
 import models, { sequelize } from './models';
+import loaders from './loaders';
 
 const app = express();
-app.use(cors())
-const getMe = async (req) => {
-    const token = req.headers['x-token']
 
-    if (token) {
-        try {
-          return await jwt.verify(token, process.env.SECRET)
-        } catch (e) {
-            throw new AuthenticationError(
-                'Your session expired, Sign in again.'
-            )
-        }
+app.use(cors());
+
+const getMe = async req => {
+  const token = req.headers['x-token'];
+
+  if (token) {
+    try {
+      return await jwt.verify(token, process.env.SECRET);
+    } catch (e) {
+      throw new AuthenticationError(
+        'Your session expired. Sign in again.',
+      );
     }
-}
+  }
+};
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
   formatError: error => {
-    // remove the internal sequelize error message
-    // leave only the important validation error
     const message = error.message
       .replace('SequelizeValidationError: ', '')
       .replace('Validation error: ', '');
@@ -42,81 +44,90 @@ const server = new ApolloServer({
     };
   },
   context: async ({ req, connection }) => {
-      if (connection) {
-          return {
-              models
-          }
-      }
-      if (req) {
-        const me = await getMe(req)
-        return {
-            models,
-            me: me,
-          secret: process.env.SECRET,
-            user: new DataLoader(keys =>
-                loaders.user.batchUsers(keys, models)),
-        }
-        }   
-    },
-  }
-);
+    if (connection) {
+      return {
+        models,
+        loaders: {
+          user: new DataLoader(keys =>
+            loaders.user.batchUsers(keys, models),
+          ),
+        },
+      };
+    }
+
+    if (req) {
+      const me = await getMe(req);
+
+      return {
+        models,
+        me,
+        secret: process.env.SECRET,
+        loaders: {
+          user: new DataLoader(keys =>
+            loaders.user.batchUsers(keys, models),
+          ),
+        },
+      };
+    }
+  },
+});
 
 server.applyMiddleware({ app, path: '/graphql' });
 
-const httpServer = http.createServer(app)
-server.installSubscriptionHandlers(httpServer)
-
-const eraseDatabaseOnSync = true;
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
 
 const isTest = !!process.env.TEST_DATABASE;
 const isProduction = !!process.env.DATABASE_URL;
 const port = process.env.PORT || 8000;
 
-sequelize.sync({ force: isTest || isProduction }).then(async () =>{
+sequelize.sync({ force: isTest || isProduction }).then(async () => {
   if (isTest || isProduction) {
-      createUsersWithMessages(new Date());
-    }
+    createUsersWithMessages(new Date());
+  }
+
   httpServer.listen({ port }, () => {
-  console.log('Apollo Server on http://localhost:${port}/graphql');
-  })
+    console.log(`Apollo Server on http://localhost:${port}/graphql`);
+  });
 });
-const createUsersWithMessages = async (date) => {
-    await models.User.create(
-      {
-        username: 'k-fang',
-        email: 'k-fang@yahoo.co.jp',
-        password: '1234qwer',
-        role: 'ADMIN',
-        messages: [
-          {
-            text: 'Published the Road to learn React',
-            createdAt: date.setSeconds(date.getSeconds() + 1),
-          },
-        ],
-      },
-      {
-        include: [models.Message],
-      },
-    );
-  
-    await models.User.create(
-      {
-        username: 'alice',
-        email: 'alice@yahoo.co.jp',
-        password: 'alicepass',
-        messages: [
-          {
-            text: 'Happy to release ...',
-            createdAt: date.setSeconds(date.getSeconds() + 1),
-          },
-          {
-            text: 'Published a complete ...',
-            createdAt: date.setSeconds(date.getSeconds() + 1),
-          },
-        ],
-      },
-      {
-        include: [models.Message],
-      },
-    );
-  };
+
+const createUsersWithMessages = async date => {
+  await models.User.create(
+    {
+      username: 'rwieruch',
+      email: 'hello@robin.com',
+      password: 'rwieruch',
+      role: 'ADMIN',
+      messages: [
+        {
+          text: 'Published the Road to learn React',
+          createdAt: date.setSeconds(date.getSeconds() + 1),
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+
+  await models.User.create(
+    {
+      username: 'ddavids',
+      email: 'hello@david.com',
+      password: 'ddavids',
+      messages: [
+        {
+          text: 'Happy to release a GraphQL in React tutorial',
+          createdAt: date.setSeconds(date.getSeconds() + 1),
+        },
+        {
+          text: 'A complete React with Apollo and GraphQL Tutorial',
+          createdAt: date.setSeconds(date.getSeconds() + 1),
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+};
